a7367454712576070ee829d9e100fba0032f49fc
3-Hz <edward.h.zhao@gmail.com>
2020-12-31 20:16:06 -0600

    built out pages a bit

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,3 +1,34 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
+
 export default function Blog() {
-  return <h1>Blog</h1>
+  return (
+    <div className={styles.container}>
+
+      <Head>
+        <title>3Hz</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.main}>
+
+        <h1 className={styles.title}>Blog</h1>
+
+        <div className={styles.grid}>
+          <a className={styles.card}>
+            WIP
+          </a>
+        </div>
+
+      </div>
+
+      <footer className={styles.footer}>
+        <Link href="/">
+          <a>Back to home &rarr;</a>
+        </Link>
+      </footer>
+
+    </div>
+  )
 }
